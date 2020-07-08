3b3de33b83d37eb953f4f2e6537b40e4381ce1e5
emmanuel <gyab12@yahoo.com>
2020-07-08 13:15:03 +0200

    added routing for login and signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { HomepageComponent } from './dealer_components/homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/homepage', pathMatch: 'full'},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
