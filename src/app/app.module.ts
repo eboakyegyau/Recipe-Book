4c25d59307ed7b67eab97635079ce6556a2ebac5
emmanuel <gyab12@yahoo.com>
2020-07-10 12:40:21 +0200

    password validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ValidateEqualModule } from 'ng-validate-equal';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,6 +36,7 @@ import { ResetComponent } from './authentication/reset/reset.component';
     BrowserModule,
     FormsModule,
     CommonModule,
+    ValidateEqualModule,
     HttpClientModule,
     ReactiveFormsModule,
     AppRoutingModule
