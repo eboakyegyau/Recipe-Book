3928465d55575640b6322be9668288bb5df3408b
emmanuel <gyab12@yahoo.com>
2020-07-13 13:58:58 +0200

    authenticated guard

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private userSub: Subscription;
+
+  isAuthenticated = false;
+
+  constructor(private authenticationService:AuthenticationService) { }
 
   ngOnInit(): void {
+
+    this.userSub = this.authenticationService.user.subscribe( user => {
+      this.isAuthenticated = !user ? false : true;
+    });
+  }
+
+  ngOnDestroy(): void {
+
+    this.userSub.unsubscribe();
   }
 
 }
