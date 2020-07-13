d78adcf002584a4c161649afbfed62a2a7c6e60e
emmanuel <gyab12@yahoo.com>
2020-07-13 12:26:19 +0200

    Added user model & errorhandler Login/SignUp/Reset

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,5 +1,8 @@
+import { User } from './../authentication/user.model';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { Subject, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 interface AuthResponseData {
 
@@ -18,6 +21,8 @@ interface AuthResponseData {
 })
 export class AuthenticationService {
 
+  user = new Subject<User>();
+  
   constructor(private http: HttpClient) { }
 
   signUp(email: string, password: string){
@@ -27,7 +32,10 @@ export class AuthenticationService {
           password: password,
           returnSecureToken: true,
       } 
-    );
+    )
+    .pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+    }));
   }
 
   logIn(email: string, password: string){
@@ -37,7 +45,10 @@ export class AuthenticationService {
          password: password,
          returnSecureToken: true
       } 
-    );
+    )
+    .pipe(catchError(this.handleError),tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn)
+    }));
  }
 
  reset(email: string){
@@ -47,5 +58,48 @@ export class AuthenticationService {
        requestType: "PASSWORD_RESET"
     } 
   );
+}
+
+private handleAuthentication(
+  email: string,
+  userId: string,
+  token: string,
+  expiresIn: number
+) {
+  const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
+  const user = new User(
+    email, 
+    userId, 
+    token, 
+    expirationDate
+    );
+    this.user.next(user);
+
+}
+
+private handleError(errorRes: HttpErrorResponse) {
+  let errorMessage = "An unknown error occured!";
+  if (!errorRes.error || !errorRes.error.error) {
+    return throwError(errorMessage);
+  }
+  switch (errorRes.error.error.message) {
+    case 'EMAIL_EXISTS':
+      errorMessage = 'Try Again';
+      break;
+
+    case 'USER_DISABLED':
+      errorMessage = 'Try Again';
+      break;
+
+    case 'INVALID_PASSWORD':
+      errorMessage = 'Try Again';
+      break;
+
+    case 'EMAIL_NOT_FOUND':
+      errorMessage = 'Try Again';
+      break;
+  }
+  return throwError(errorMessage);
+
 }
 }
