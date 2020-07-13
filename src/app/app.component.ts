28275527352ceca6bdc9df946a0d8c2193b0a47d
emmanuel <gyab12@yahoo.com>
2020-07-13 15:37:08 +0200

    added autoLogin and claer local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './services/authentication.service';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Auto-Handler';
+
+  constructor(private authenticationService: AuthenticationService){}
+
+  ngOnInit() {
+    this.authenticationService.autoLogin();
+  }
 }
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -62,6 +62,31 @@ export class AuthenticationService {
 
 logOut() {
   this.user.next(null);
+  localStorage.clear();
+}
+
+autoLogin() {
+  const userData: {
+    email: string;
+    id: string;
+    _token: string;
+    _tokenExpirationDate: string;
+  } = JSON.parse(localStorage.getItem('userData'));
+  if(!userData) {
+    return;
+  }
+
+  const loadedUser = new User(
+    userData.email,
+    userData.id,
+    userData._token,
+    new Date(userData._tokenExpirationDate)
+  );
+
+  if(loadedUser.token) {
+    this.user.next(loadedUser);
+
+  }
 }
 
 private handleAuthentication(
@@ -78,6 +103,8 @@ private handleAuthentication(
     expirationDate
     );
     this.user.next(user);
+
+    localStorage.setItem('userData', JSON.stringify(user));
 
 }
 
