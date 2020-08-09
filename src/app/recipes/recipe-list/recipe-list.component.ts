db321f63f914dd985a8c03aa47fa30c040131ec3
emmanuel <gyab12@yahoo.com>
2020-08-09 15:26:45 +0200

    added ingredient model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[]  = [
     new Recipe('Recipe A','Test Recipe for A','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Recipe A','Test Recipe for A','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('Recipe B','Test Recipe for B','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
   ];
 
   constructor() { }
